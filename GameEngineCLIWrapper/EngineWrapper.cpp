fae85a407f65801d02eb3817a1cc9c71d4b9bc97
Jack <jacklaing1234@gmail.com>
2025-03-29 19:33:08 +0000

    Completed basic setup

--- GameEngineCLIWrapper/EngineWrapper.cpp
+++ GameEngineCLIWrapper/EngineWrapper.cpp
@@ -12,7 +12,5 @@ EngineWrapper::~EngineWrapper() {
 
 void EngineWrapper::Run(IntPtr hwnd) {
     // Pass the HWND to your SFML window creation
-    sf::RenderWindow window((HWND)hwnd.ToPointer());
 
-    engine->run(window); // Assuming your GameEngine accepts sf::RenderWindow&
 }
